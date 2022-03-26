e492f066bcf1908fab916a0e2121fc374eb310fe
sylvestre <pasytchangwa@gmail.com>
2022-03-26 10:22:06 +0200

    Add reducer file

---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'INCREASE'
+    case 'DECREASE'
+    case 'REMOVE'
+    case 'REMOVE_ALL'
+    default
+  }
+  return (
+    <div>reducer</div>
+  )
+}
+
+export default reducer
