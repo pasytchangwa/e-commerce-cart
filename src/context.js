b77f538371c827046ddbb8c3018a6bddc6020ba7
sylvestre <pasytchangwa@gmail.com>
2022-03-26 15:31:12 +0200

    Add usecontex global

--- src/context.js
+++ src/context.js
@@ -1,16 +1,64 @@
-import React, {useState, useEffect, useContext, createContext} from 'react'
+import React, {useEffect, useContext, useReducer, createContext} from 'react';
+import cartItems from './data';
+import reducer from './reducer';
 
 const url = 'https://course-api.com/react-useReducer-cart-project'
 
 const AppContext = createContext()
 
-const AppProvider = () => {
-  const [cart, setCart] = useState([])
+const initialState = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+  isLoading: false,
+}
+
+const AppProvider = ({children}) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const clearCart = () => {
+    dispatch({type: 'CLEAR_CART'})
+  }
+
+  const increase = (id) => {
+    dispatch({type: 'INCREASE', payload: id})
+  }
+
+  const decrease = (id) => {
+    dispatch({type: 'DECREASE', payload: id})
+  }
+
+  const remove = (id) => {
+    dispatch({type: 'REMOVE', payload: id})
+  }
+
+  const fetchData = async () => {
+    dispatch({type: 'LOADING'})
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      dispatch({type: 'FETCH_DATA_SUCCESS', payload: data})
+    } catch (error) {
+      dispatch({type: 'FETCH_DATA_FAILURE', paylod: error})
+    }
+  }
+
+  const toggleAmount = (id, type) => {
+    dispatch({type: 'TOGGLE_AMOUNT', payload: {id, type}})
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    dispatch({type: 'CALCULATE_TOTAL'})
+  }, [state.cart])
 
   return (
-   <AppContext.Provider>
-
-   </AppContext.Provider>
+    <AppContext.Provider value={{...state,clearCart,remove,increase,decrease,toggleAmount}}>
+      {children}
+    </AppContext.Provider>
   )
 }
 
